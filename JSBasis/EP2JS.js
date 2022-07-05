055cb9afb18db7d2be11c6f79267fdedad82734f
gasader <kajorhkaen@gmail.com>
2022-07-06 03:16:44 +0700

    แนทททระ

--- JSBasis/EP2JS.js
+++ JSBasis/EP2JS.js
@@ -77,3 +77,29 @@ else if(time < 18){
 else {
     console.log("good evening");
 }
+
+switch(new Date().getDay()){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+  
+    default:
+}
