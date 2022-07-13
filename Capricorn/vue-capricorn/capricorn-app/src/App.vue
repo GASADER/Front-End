f7aa1bc5e37e1c9a0203ecfc0cb74a6d67613e62
gasader <kajorhkaen@gmail.com>
2022-07-13 17:42:56 +0700

    New

---
+++ Capricorn/vue-capricorn/capricorn-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1>{{firstName}}</h1>
+    <h2>{{800+200}}</h2>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      firstName:"jo",
+      lastName:"save",
+      age:15
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
