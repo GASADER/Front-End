22480cb6e091715449e25ebc1f82a813adfb8c3a
GASADER <kajorhkaen@gmail.com>
2022-07-21 16:18:53 +0700

    add Component

--- my-app-component/src/App.vue
+++ my-app-component/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <h1>156</h1>
+      <list-data></list-data>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import ListData from './components/ListData.vue'
+export default{
+  name:"App",
+  components:{
+    ListData
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ my-app-component/src/components/ListData.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{message}}</div>
+</template>
+
+<script>
+export default {
+    name:"ListData",
+    data(){
+        return{
+            message:"ListData Component"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
