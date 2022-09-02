486201e4b40effbd07c405294a4356def086a7d2
GASADER <kajorhkaen@gmail.com>
2022-09-03 02:40:33 +0700

    Closures: Data privacy

--- JSBasis/Closures.js
+++ JSBasis/Closures.js
@@ -1,8 +1,42 @@
+// function outer(){
+//     const name = 'Outer';
+//     function inner(){
+//         console.log('Outer name:',name);
+//     }
+//     inner();
+// }
+// outer();
+
 function outer(){
     const name = 'Outer';
-    function inner(){
+    return function inner(){
         console.log('Outer name:',name);
     }
-    inner();
 }
-outer();
+const innerFunction = outer();
+innerFunction();
+
+// Data privacy
+
+// let counter = 0;
+// function time(){
+//     counter += 1;
+//     console.log('Counter:',counter);
+// }
+// time();
+// time();
+// time();
+// time();
+
+function createTime(){
+    let counter = 0;
+    return function time(){
+        counter += 1;
+        console.log('Counter:',counter);
+    }
+}
+const times = createTime();
+times();
+times();
+times();
+times();
